eb97705b73597185849286d82648601c4f19794a
Rachel <rachel.rolon@gmail.com>
2020-10-01 20:48:32 -0500

    studio 3: one part of bonus

---
+++ Studio3/Studio3/Count.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Studio3
+{
+    class Count
+    {
+        public static Dictionary<char, int> CountChars(string input)
+        {
+            input = input.ToUpper();
+            Dictionary<char, int> characterCounts = new Dictionary<char, int>();
+            char[] charactersInStr = input.ToCharArray();
+            
+            foreach (char item in charactersInStr)
+            {
+                if (characterCounts.ContainsKey(item))
+                {
+                    characterCounts[item] ++;
+                }
+                else
+                {
+                    characterCounts.Add(item, 1);
+                }
+            }
+
+            return characterCounts;
+        }
+    }
+}
---
+++ Studio3/Studio3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Studio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+
+            Dictionary<char, int> countedCharacters = Count.CountChars(text);
+            foreach (KeyValuePair<char, int> item in countedCharacters)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+        }
+    }
+}
